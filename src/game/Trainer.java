5e2a933454d4d7524f59b315bf1cf8cbdc12d494
azdunek <agnieszka95zdunek@gmail.com>
2019-11-16 16:33:06 +0100

    Added shouts list and method returning random one.

--- src/game/Trainer.java
+++ src/game/Trainer.java
@@ -3,27 +3,32 @@ package game;
 import pokemons.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Trainer {
     private String name;
     private ArrayList<Pokemon> pokemons;
+    private Scanner scanner = new Scanner(System.in);
+    private ArrayList<String> shouts;
 
-    public Trainer(String name) {
+    Trainer(String name) {
         this.name = name;
         this.pokemons = new ArrayList<Pokemon>();
-        pokemons.add(pikachu);
-        pokemons.add(squirtle);
-        pokemons.add(charmander);
-        pokemons.add(bulbasaur);
+        pokemons.add(new Pikachu());
+        pokemons.add(new Squirtle());
+        pokemons.add(new Charmander());
+        pokemons.add(new Bulbasaur());
+        this.shouts = new ArrayList<>();
+        shouts.add("I choose you!");
+        shouts.add("You will be my Pokemon! ");
+        shouts.add("Fight with me!");
+        shouts.add("Stand up and fight!");
     }
 
 
-    public String getName() {
+    String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public ArrayList<Pokemon> getPokemons() {
@@ -34,21 +39,38 @@ public class Trainer {
         this.pokemons = pokemons;
     }
 
-    public Pokemon chooseRandomPokemon(){
-        int number =randomWithRange(0,pokemons.size()-1);
+    Pokemon chooseRandomPokemon() {
+        int number = randomWithRange(0, pokemons.size() - 1);
         return pokemons.get(number);
     }
 
+    String getRandomShout() {
+        int number = randomWithRange(0, shouts.size() - 1);
+        return shouts.get(number);
+    }
 
-    int randomWithRange(int min, int max) {
+    Pokemon selectPokemon() {
+        System.out.println("Choose your pokemon!");
+        displayPokemonList();
+
+        int number = scanner.nextInt();
+        while (number < 0 || number >= pokemons.size()) {
+            System.out.println("Error! Choose again!");
+            number = scanner.nextInt();
+        }
+        return pokemons.get(number);
+    }
+
+    private void displayPokemonList() {
+        for (int i = 0; i < pokemons.size(); i++) {
+            System.out.println(i + " " + pokemons.get(i).getClass().getSimpleName());
+        }
+    }
+
+
+    private int randomWithRange(int min, int max) {
         int range = (max - min) + 1;
         return (int) (Math.random() * range) + min;
     }
-
-    Pokemon pikachu = new Pikachu();
-    Pokemon squirtle = new Squirtle();
-    Pokemon charmander = new Charmander();
-    Pokemon bulbasaur =new Bulbasaur();
-
-
 }
+
